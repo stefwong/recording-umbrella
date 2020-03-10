cdb0b8e77db59fd2c8f91808d6a0525900503488
Vyacheslav Starkov <vyacheslavstarkov@Vyacheslavs-MacBook-Pro.local>
2020-03-10 14:49:17 -0400

    added a component file

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ItemsGrid from './components/ItemsGrid'
 
 function App() {
   return (
---
+++ client/src/components/ItemsGrid.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+class ItemsGrid extends Component {
+  constructor() {
+
+  }
+  render() {
+    return (
+      <h1>Items Grid Component</h1>
+    )
+  }
+
+}
+
+
+export default ItemsGrid
