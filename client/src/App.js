7f0c4a0e3f4bb92a75bc53b0581a902928a3096b
stefwong <hifromstef@gmail.com>
2020-03-11 10:52:29 -0400

    added items screen grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ItemsGrid from './components/ItemsGrid'
+import ItemsScreen from './screens/ItemsScreen'
 
 function App() {
   return (
     <div className="App">
       Survival App
+      <ItemsScreen />
     </div>
   );
 }
