8cf293ef85d95fc4eaaa28d26b0c57b7dd7b8d10
Vyacheslav Starkov <vyacheslavstarkov@Vyacheslavs-MacBook-Pro.local>
2020-03-11 11:04:01 -0400

    added a component and some lines of code for Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import ItemsGrid from './components/ItemsGrid'
+import InventoryForm from './components/InventoryForm'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 function App() {
   return (
-    <div className="App">
-      Survival App
+      <div className="App">
+        Survival App
     </div>
   );
 }
