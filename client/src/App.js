1f5ad7edbce0182b200a4dafa432ed3ec4b8d06d
C <c@Cs-MacBook-Air.local>
2020-03-11 11:51:12 -0400

    test changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import ItemsGrid from './components/ItemsGrid'
+import Header from './components/Header'
 
 function App() {
   return (
+    <>
     <div className="App">
       Survival App
     </div>
+    <Header/>
+    </>
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {NavLink, Link} from 'react-router-dom'
+ import Button from '@material-ui/core/Button';
+
+
+function Header (){
+  return(
+    <div className="Header">
+      <div className='links'>
+        <NavLink
+        exact
+        to={"/"}
+        activeClassName="active">
+          <Button>
+          Upper Right Corner Button
+          </Button>
+        </NavLink>
+
+        <NavLink
+        to={"/loggedin"}
+        activeClassName="active">
+          <Button>
+          Upper Right Corner Button 2
+          </Button>
+        </NavLink>
+        </div>
+      <h1>Let's get prepared!</h1>
+      <h3>Welcome to the Survival App!</h3>
+
+      <div className="middle-buttons">
+          <Link 
+          exact
+          to={"/blahblah"}
+          activeClassName="active">
+            <Button>Middle Button 1</Button>
+          </Link>
+          <Link
+          exact
+          to="/blahblahblahba"
+          activeClassName="active">
+            <Button>Middle Button 2</Button>
+          </Link>
+    </div>
+    </div>
+  )
+}
+
+export default Header; 
