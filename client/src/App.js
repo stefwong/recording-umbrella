333eb27064e9321999cd4878c875dfd803b676d2
stefwong <hifromstef@gmail.com>
2020-03-15 14:53:31 -0400

    added app menu search bar to support client-side routing and placeholder components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import ItemsGrid from './components/ItemsGrid'
 import ItemsScreen from './screens/ItemsScreen'
+import PrimarySearchAppBar from './components/PrimarySearchAppBar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from "react-router-dom";
+import UserStoreFrontEdit from './screens/UserStoreFrontEdit';
+import MarketPlaceSearchResults from './screens/MarketPlaceSearchResults';
 
 function App() {
+
+     
   return (
     <div className="App">
-      Survival App
-      <ItemsScreen />
+      <Router>
+        <PrimarySearchAppBar />
+       
+        <Switch>
+          <Route exact path="/UserStoreFrontEdit" render={props => (<UserStoreFrontEdit {...props}/>)} />
+          <Route exact path="/home" render={ props => (<ItemsScreen {...props} />)} />
+          <Route exact path="/MarketPlaceSearchResults/:searchText" render={ props => (<MarketPlaceSearchResults {...props}/>)} />
+          <Route exact path="/" render={ props => (<ItemsScreen {...props} />)} />
+          
+        </Switch>
+      </Router>
+
     </div>
   );
 }
